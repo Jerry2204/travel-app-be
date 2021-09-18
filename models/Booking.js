d773cbc46c0c710bd0ae97e38474aba466d03969
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2021-09-18 21:50:57 +0700

    create model bookin

---
+++ models/Booking.js
@@ -0,0 +1,61 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+const { ObjectId } = mongoose.Schema;
+
+const bookingSchema = new Schema({
+  bookingStartDate: {
+    type: Date,
+    required: true,
+  },
+  bookingEndDate: {
+    type: Date,
+    required: true,
+  },
+  itemId: [
+    {
+      _id: {
+        type: ObjectId,
+        ref: "Item",
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+      night: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  memberId: [
+    {
+      type: ObjectId,
+      ref: "Member",
+    },
+  ],
+  bankId: [
+    {
+      type: ObjectId,
+      ref: "Bank",
+    },
+  ],
+  proofPayment: {
+    type: String,
+    required: true,
+  },
+  bankFrom: {
+    type: String,
+    required: true,
+  },
+  accountHolder: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Booking", bookingSchema);
