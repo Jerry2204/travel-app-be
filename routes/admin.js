80bb7633d99d3342cfa00ad0c64727f96a44e4f6
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2021-09-28 14:12:44 +0700

    create view category

--- routes/admin.js
+++ routes/admin.js
@@ -4,5 +4,6 @@ const adminController = require("../controllers/adminController");
 
 /* GET home page. */
 router.get("/dashboard", adminController.viewDashboard);
+router.get("/category", adminController.viewCategory);
 
 module.exports = router;
