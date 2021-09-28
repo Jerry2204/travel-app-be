8c3a221ae340db4c932747c4bb90bff3d1b11ebd
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2021-09-28 14:28:11 +0700

    create view booking

--- routes/admin.js
+++ routes/admin.js
@@ -7,5 +7,6 @@ router.get("/dashboard", adminController.viewDashboard);
 router.get("/category", adminController.viewCategory);
 router.get("/bank", adminController.viewBank);
 router.get("/item", adminController.viewItem);
+router.get("/booking", adminController.viewBooking);
 
 module.exports = router;
