e2f8a7e23c652a88cda10173098d7f33acfe480f
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2021-10-28 14:57:11 +0700

    finishing delete category

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -4,15 +4,18 @@ module.exports = {
   viewDashboard: (req, res) => {
     res.render("admin/dashboard/view_dashboard");
   },
+
   viewCategory: async (req, res) => {
     const category = await Category.find();
     res.render("admin/category/view_category", { category });
   },
+
   addCategory: async (req, res) => {
     const { name } = req.body;
     await Category.create({ name });
     res.redirect("/admin/category");
   },
+
   editCategory: async (req, res) => {
     const { id, name } = req.body;
     const category = await Category.findOne({ _id: id });
@@ -20,12 +23,23 @@ module.exports = {
     await category.save();
     res.redirect("/admin/category");
   },
+
+  deleteCategory: async (req, res) => {
+    const { id } = req.params;
+    const category = await Category.findOne({ _id: id });
+    console.log(category);
+    await category.remove();
+    res.redirect("/admin/category");
+  },
+
   viewBank: (req, res) => {
     res.render("admin/bank/view_bank");
   },
+
   viewItem: (req, res) => {
     res.render("admin/item/view_item");
   },
+
   viewBooking: (req, res) => {
     res.render("admin/booking/view_booking");
   },
--- routes/admin.js
+++ routes/admin.js
@@ -4,9 +4,13 @@ const adminController = require("../controllers/adminController");
 
 /* GET home page. */
 router.get("/dashboard", adminController.viewDashboard);
+
+/* Endpoint For Category */
 router.get("/category", adminController.viewCategory);
 router.post("/category", adminController.addCategory);
 router.put("/category", adminController.editCategory);
+router.delete("/category/:id", adminController.deleteCategory);
+
 router.get("/bank", adminController.viewBank);
 router.get("/item", adminController.viewItem);
 router.get("/booking", adminController.viewBooking);
