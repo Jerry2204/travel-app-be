e4b090104918b08755a165886e1a3246f4e44125
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2022-05-11 22:58:31 +0700

    view data feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -373,10 +373,12 @@ module.exports = {
         message: alertMessage,
         status: alertStatus,
       };
+      const feature = await Feature.find({ itemId: itemId });
       res.render('admin/item/detail_item/view_detail_item', {
         title: 'Staycation | Detail Item',
         alert,
         itemId,
+        feature,
       });
     } catch (error) {
       req.flash('alertMessage', `${error.message}`);
