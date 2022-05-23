3b54b414060cf3ade566af21a74ff34f79c46ae3
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2022-05-23 21:39:06 +0700

    delete activity

--- routes/admin.js
+++ routes/admin.js
@@ -32,6 +32,7 @@ router.put('/item/update/feature', uploadSingle, adminController.editFeature);
 router.delete('/item/:itemId/feature/:id', adminController.deleteFeature);
 router.post('/item/add/activity', uploadSingle, adminController.addActivity);
 router.put('/item/update/activity', uploadSingle, adminController.editActivity);
+router.delete('/item/:itemId/activity/:id', adminController.deletActivity);
 
 router.get('/booking', adminController.viewBooking);
 
