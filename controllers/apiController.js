383581505a34234d95a1d911a58adc5bbec74af2
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2022-06-14 23:15:17 +0700

    API: get item category

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,6 +1,7 @@
 const Item = require('../models/Item');
 const Treasure = require('../models/Activity');
 const Booking = require('../models/Booking');
+const Category = require('../models/Category');
 
 module.exports = {
   landingPage: async (req, res) => {
@@ -9,6 +10,20 @@ module.exports = {
         .select('_id title country city price unit imageId')
         .limit(5)
         .populate({ path: 'imageId', select: '_id imageUrl' });
+
+      const categories = await Category.find()
+        .select('_id name')
+        .limit(3)
+        .populate({
+          path: 'itemId',
+          select: '_id title country city isPopular imageId',
+          perDocumentLimit: 4,
+          populate: {
+            path: 'imageId',
+            select: '_id imageUrl',
+            perDocumentLimit: 1,
+          },
+        });
 
       const treasure = await Treasure.find();
       const booking = await Booking.find();
@@ -21,6 +36,7 @@ module.exports = {
           cities: city.length,
         },
         mostPicked,
+        category: categories,
       });
     } catch (error) {}
   },
